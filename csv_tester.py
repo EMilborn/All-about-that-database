26eee0814451340017f86b5ea82f6158b7d32b6f
Harry Lum <hlum1@stuy.edu>
2016-10-14 12:24:25 -0400

    a

---
+++ csv_tester.py
@@ -0,0 +1,11 @@
+import csv
+
+fObj = open("peeps.csv") 
+d=csv.DictReader(fObj)
+
+for k in d:
+    print k['name'] + " " + k['id']
+	
+
+
+    
---
+++ db_builder.py
@@ -0,0 +1,37 @@
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitates CSV I/O
+
+
+f="discobandit.db"
+
+db = sqlite3.connect(f) #open if f exists, otherwise create
+c = db.cursor()    #facilitate db ops
+
+#==========================================================
+#INSERT YOUR POPULATE CODE IN THIS ZONE
+#...perhaps by beginning with these examples...
+
+
+q = "CREATE TABLE students (name TEXT, id INTEGER)"
+
+c.execute(q)    #run SQL query
+
+fObj = open("peeps.csv") 
+d=csv.DictReader(fObj)
+
+for k in d:
+	c.execute("INSERT INTO students VALUES(" + d['name'] +"," + d['id']+ ')')
+	
+
+'''
+
+q = "CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER)"
+
+c.execute(q)
+'''
+
+#==========================================================
+db.commit() #save changes
+db.close()  #close database
+
+
